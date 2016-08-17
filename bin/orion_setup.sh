f18277aa694c57f5ae9474073ca38a82c7e4a443
BoykoAlex <aboyko@pivotal.io>
2016-08-17 12:57:53 -0400

    Setup compile script to use orion setup script

--- bin/orion_setup.sh
+++ bin/orion_setup.sh
@@ -1,19 +1,15 @@
-echo "Intalling Orion from separate script"
+echo "Setting up Orion from separate script"
 appDir=$1
 cacheDir=$2
 orionDir=$appDir/orion
 ##############################################################################################
 # Orion setup and launch
 ##############################################################################################
-echo ===== setting up Orion =====
-cd $appDir
-if [-d ${orionDir}]; then
-    echo "Found Orion"
-else
+echo ===== Setting up Orion =====
+if [! -d ${orionDir}]; then
     mkdir ${orionDir}
-    cd ${orionDir}
-    npm install orion
 fi
-
+cd ${orionDir}
+npm install orion
 cd ${orionDir}/node_modules/orion
 npm start orion
