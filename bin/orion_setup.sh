5aab5f28914308cb14595b025f53be2ba76e727a
BoykoAlex <aboyko@pivotal.io>
2016-08-17 16:54:05 -0400

    Correct orion version

--- bin/orion_setup.sh
+++ bin/orion_setup.sh
@@ -11,7 +11,7 @@ echo ===== Setting up Orion =====
 if [ ! -d ${orionDir} ]; then
     mkdir ${orionDir}
 fi
-cd ${orionDir}@0.0.90
-${npm} install orion
+cd ${orionDir}
+${npm} install orion@0.0.90
 cd ${orionDir}/node_modules/orion
 ${npm} start orion
